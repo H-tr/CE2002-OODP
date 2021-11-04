733ad04b3f2d20f2ba33761d44a324cd30e8b3c7
Chiam Chuen <chiamchuen@gmail.com>
2021-11-04 22:53:23 +0800

    Broken file storage code in Order Manager

--- Classes/RestaurantSystem/src/App.java
+++ Classes/RestaurantSystem/src/App.java
@@ -4,36 +4,36 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws IOException 
-    {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
         /*----------------------------- Getting info from Staff--------------------------------*/
 
         Restaurant restaurant = new Restaurant();
 
-
         System.out.println("Welcome to Restaurant Reservation and Point of Sale System (RRPSS)");
         System.out.println("Please input your staff ID: ");
 
         int staff_ID = sc.nextInt();
 
-        //Check restaurant if there is a Staff with this ID?
-        //If have: Retrieve information from restaurant
-        //else: Get infor from user
+        // Check restaurant if there is a Staff with this ID?
+        // If have: Retrieve information from restaurant
+        // else: Get infor from user
 
         System.out.println("Please input your name and gender: ");
-        
+
         String name = sc.next();
         String gender = sc.next();
 
         System.out.println("Please input your job title: ");
 
-        String jobtitle =sc.next();
-        
+        String jobtitle = sc.next();
+
         Staff staff = new Staff(name, gender, staff_ID, jobtitle);
 
-        //Add Staff to Restaurant
+        // Add Staff to Restaurant
+
+        TableManager.getList();
 
         /*----------------------------- Switch case for Functional Requirements--------------------------------*/
 
@@ -49,122 +49,117 @@ public class App {
             System.out.println("Please enter your choice: ");
             choice = sc.nextInt();
 
-            switch(choice){
+            switch (choice) {
+            case 1:
+                MenuManager.menuManage();
+                break;
+            case 2:
+                TableManager.tableManage();
+                break;
+            case 3:
+                int ch;
+                System.out.println("[1] Create an order");
+                System.out.println("[2] Create a reservation");
+                System.out.println("[3] Remove a reservation");
+                System.out.println("[4] Finish an order");
+                System.out.println("[5] View reservations");
+                System.out.println("[6] View orders");
+                System.out.println("[7] edit reservation");
+                System.out.println("[8] edit orders");
+                System.out.println("[9] Remove past reservations");
+                System.out.println("Please enter your choice: ");
+                ch = sc.nextInt();
+                switch (ch) {
                 case 1:
-                    MenuManager.menuManage();
+                    System.out.println("Do you have a reservation? [1]yes [2]no");
+                    int hasRes = sc.nextInt();
+                    switch (hasRes) {
+                    case 1:
+                        // DONE in the restaurant, find out the corresponding reservation, check whether
+                        // it is valid (the time). If valid, change that event from reservation to
+                        // order, otherwise, return error message
+                        Event reservation = restaurant.searchReservation();
+                        if (reservation == null) {
+                            System.out.println("ERROR: No reservation was made under this name.");
+                            break;
+                        }
+                        if (!restaurant.checkCurrentDate(reservation)) // Check if reservation for today
+                        {
+                            System.out.println("ERROR: This reservation is not for today!");
+                            break;
+                        } else {
+                            restaurant.deleteReservation(reservation);
+                            // DONE Reservation found and take the order
+                            restaurant.createOrder_reserved(reservation, staff);
+
+                        }
+                        break;
+                    case 2:
+                        restaurant.createOrder_unreserved(staff);
+                        // DONE create the order in restaurant. Use the table(Table) and date(Timing)
+                        break;
+                    }
                     break;
                 case 2:
-                    TableManager.tableManage();
+                    // DONE create the reservation in restaurant. Use the table(Table) and
+                    // time(Timing)
+                    restaurant.createReservation();
                     break;
                 case 3:
-                    int ch;
-                    System.out.println("[1] Create an order");
-                    System.out.println("[2] Create a reservation");
-                    System.out.println("[3] Remove a reservation");
-                    System.out.println("[4] Finish an order");
-                    System.out.println("[5] View reservations");
-                    System.out.println("[6] View orders");
-                    System.out.println("[7] edit reservation");
-                    System.out.println("[8] edit orders");
-                    System.out.println("[9] Remove past reservations");
-                    System.out.println("Please enter your choice: ");
-                    ch = sc.nextInt();
-                    switch(ch){
-                        case 1:
-                            System.out.println("Do you have a reservation? [1]yes [2]no");
-                            int hasRes = sc.nextInt();
-                            switch(hasRes) {
-                                case 1:
-                                    // DONE in the restaurant, find out the corresponding reservation, check whether it is valid (the time). If valid, change that event from reservation to order, otherwise, return error message
-                                    Event reservation = restaurant.searchReservation();
-                                    if (reservation == null){
-                                        System.out.println("ERROR: No reservation was made under this name.");
-                                        break;
-                                    }
-                                    if (!restaurant.checkCurrentDate(reservation))      //Check if reservation for today
-                                    {
-                                        System.out.println("ERROR: This reservation is not for today!");
-                                        break;
-                                    }
-                                    else{
-                                        restaurant.deleteReservation(reservation);
-                                        // DONE Reservation found and take the order
-                                        restaurant.createOrder_reserved(reservation, staff);
-
-                                    }
-                                    break;
-                                case 2:
-                                    restaurant.createOrder_unreserved(staff);
-                                    // DONE create the order in restaurant. Use the table(Table) and date(Timing)
-                                    break;
-                            }
-                            break;
-                        case 2:
-                            // DONE create the reservation in restaurant. Use the table(Table) and time(Timing)
-                            restaurant.createReservation();
-                            break;
-                        case 3:
-                            Event reservation = restaurant.searchReservation();
-                            restaurant.deleteReservation(reservation);
-                            // DONE do it with restaurant
-                            break;
-                        case 4:
-                            Event order = restaurant.searchOrder();
-                            if (order == null)
-                            {
-                                System.out.println("=====Unable to find order!=====");
-                                break;
-                            }
-                            restaurant.finaliseOrder(order, staff);
-                            restaurant.deleteOrder(order);
-                            // TODO Save (Event) order into a text file somewhere (CHIAM CHUEN PLS)
-                            break;
-                        case 5:
-                            restaurant.printReservations();
-                            break;
-                        case 6:
-                            restaurant.printOrders();
-                            break;
-
-                        case 7: //edit reservation
-                            Event reservation_1 = restaurant.searchReservation();
-                            if (reservation_1 == null)
-                            {
-                                System.out.println("ERROR: No Reservation under this name was found.");
-                                break;
-                            }
-                            restaurant.editReservation(reservation_1);
-                            break;
-
-                            
-                        case 8: //edit Order
-                            Event reservation_2 = restaurant.searchOrder();
-                            if (reservation_2 == null)
-                            {
-                                System.out.println("No Order under this name was found.");
-                                break;
-                            }
-                            restaurant.editOrder(reservation_2);
-                            break;
-
-                        case 9: //Remove past reservations that is past today
-                            restaurant.cleanReservation();
-                            break;
-                            
-                        
+                    Event reservation = restaurant.searchReservation();
+                    restaurant.deleteReservation(reservation);
+                    // DONE do it with restaurant
+                    break;
+                case 4:
+                    Event order = restaurant.searchOrder();
+                    if (order == null) {
+                        System.out.println("=====Unable to find order!=====");
+                        break;
                     }
-
-
-                 case 4:
-                 
-                    //Retrieve Orders from Revenue report
-                    //Display individual sale items
-                    //Total revenue
-
+                    restaurant.finaliseOrder(order, staff);
+                    restaurant.deleteOrder(order);
+                    // TODO Save (Event) order into a text file somewhere (CHIAM CHUEN PLS)
+                    break;
+                case 5:
+                    restaurant.printReservations();
+                    break;
+                case 6:
+                    restaurant.printOrders();
                     break;
 
+                case 7: // edit reservation
+                    Event reservation_1 = restaurant.searchReservation();
+                    if (reservation_1 == null) {
+                        System.out.println("ERROR: No Reservation under this name was found.");
+                        break;
+                    }
+                    restaurant.editReservation(reservation_1);
+                    break;
+
+                case 8: // edit Order
+                    Event reservation_2 = restaurant.searchOrder();
+                    if (reservation_2 == null) {
+                        System.out.println("No Order under this name was found.");
+                        break;
+                    }
+                    restaurant.editOrder(reservation_2);
+                    break;
+
+                case 9: // Remove past reservations that is past today
+                    restaurant.cleanReservation();
+                    break;
+
+                }
+
+            case 4:
+
+                // Retrieve Orders from Revenue report
+                // Display individual sale items
+                // Total revenue
+
+                break;
+
             }
-            
 
         } while (choice < 5);
     }

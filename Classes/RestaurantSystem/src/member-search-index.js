db030c1c794e647d04c1af8e9a9538d508558aa4
iamtianrun <tianrunhu@gmail.com>
2021-11-06 14:00:20 +0800

    add javadoc

--- Classes/RestaurantSystem/src/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();

3b858734c464246c5a0bff0776147cf3a9719a85
iamtianrun <tianrunhu@gmail.com>
2021-11-13 09:55:10 +0800

    modified:   Classes/RestaurantSystem/src/App.java

--- Classes/RestaurantSystem/src/App.java
+++ Classes/RestaurantSystem/src/App.java
@@ -1,15 +1,16 @@
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class App {
-    
-    /** 
-     * App is the user interface for staff to use. It is divided into three parts: restaurant management, customer service, and display revenue report. Considering if a staff login this system, he can choose manage the restaurant resources, serve customer, or display revenue. If he manage the resources, he can handle menu and restaurant. If he serve the customer, he can create an order, reservation, terminal the order, cancel the reservation or so on.
+/**
+ * App is the user interface for staff to use. It is divided into three parts: restaurant management, customer service, and display revenue report. Considering if a staff login this system, he can choose manage the restaurant resources, serve customer, or display revenue. If he manage the resources, he can handle menu and restaurant. If he serve the customer, he can create an order, reservation, terminal the order, cancel the reservation or so on.
      * @author Ian Chan
      * @author Hu Tianrun
      * @author Chiam Chuen
+ */
+public class App {
+    
+    /** 
      * @param args
      * @throws IOException
      * @throws ParseException
